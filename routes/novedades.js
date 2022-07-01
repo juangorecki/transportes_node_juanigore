02fcbbfbffb37357cb49bf0373cf0fedad2745dd
juan gore <juangorecki@gmail.com>
2022-07-01 08:05:28 -0300

    ruteo

---
+++ routes/novedades.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req,res,next){
+    res.render('novedades',{
+        isNovedades:true
+    });
+})
+
+
+
+
+module.exports = router;
