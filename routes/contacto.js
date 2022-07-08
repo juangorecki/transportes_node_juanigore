fa5da0b4038f313586cbb97255f8345810ac99f7
juan gore <juangorecki@gmail.com>
2022-07-08 10:32:24 -0300

    envio de mail

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,10 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer= require('nodemailer')
 
 router.get('/', function(req,res,next){
     res.render('contacto',{
         isContacto: true
     });
+})
+
+router.post('/', async function(req,res,next){
+//console.log(req.body)
+var nombre = req.body.nombre;
+var email = req.body.email;
+var tel= req.body.tel;
+var comentarios = req.body.comentarios; 
+//console.log(req.body.nombre)
+
+var obj = {
+    to:'juangorecki@gmail.com',
+    subject:'contacto desde la pagina web',
+    html: nombre + "se contacto a traves de la web  y quiere saber mas info a este correo: " + email + ".<br> su tel es: " + tel + ". su conmentario es: " + comentarios
+}
+var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    }
+})
+
+var info = await transport.sendMail(obj);
+
+res.render('contacto',{
+    message: 'mensaje enviado correctamente',
+    isContacto: true
+})
+
 })
 
 
