fe305751c94695637dbfe41db289a9f11fb51458
juan gore <juangorecki@gmail.com>
2022-07-25 09:56:20 -0300

    diseño parte 1 novedades admin

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var usuariosModel = require('./../../models/usuariosModel');
+//var usuariosModel = require('./../../models/usuariosModel');
 
-router.get('/', function(req,res,next){
-    res.render('admin/novedades',{
-        layout:'admin/layout',
+router.get('/', function(req,res,next) {
+    res.render('admin/novedades', {
+        layout: 'admin/layout',
         persona:req.session.nombre
     });
 });
