3fc80c24ce53a6ccff5c1fc59ec95fa3ec1e76e2
juan gore <juangorecki@gmail.com>
2022-07-26 06:21:14 -0300

    tabla novedades-listar

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModels')
 
+router.get('/', async function(req,res,next) {
+    var novedades = await novedadesModel.getNovedades();
 
-router.get('/', function(req,res,next) {
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        persona:req.session.nombre
+        persona:req.session.nombre,
+        novedades
     });
 });
 
