6c0b82c40b8d71785fa93711c5076584764509db
juan gore <juangorecki@gmail.com>
2022-07-28 01:29:21 -0300

    novedades desde bd en el front

--- routes/novedades.js
+++ routes/novedades.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../models/novedadesModels');
 
-router.get('/', function(req,res,next){
+router.get('/', async function(req,res,next){
+    var novedades= await novedadesModel.getNovedades();
+
+
     res.render('novedades',{
-        isNovedades:true
+        isNovedades:true,
+        novedades
     });
 })
 
